dd977eec8acfb01a19f3e342cf8ff1d9c829a862
Anton Korotkov <tohawws777@gmail.com>
2023-03-06 00:05:08 +0600

    add Example 005 Condition if else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ползователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "антон")
+{
+  Console.Write("Ура, это же АНТОН!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}

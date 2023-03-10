9c7e5c43c600319de86113cc720118365dbbea80
Anton Korotkov <tohawws777@gmail.com>
2023-03-10 21:35:46 +0600

    Задания лекции 2

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+  int lenght = collection.Length;
+  int index = 0;
+  while (index < lenght)
+  {
+
+    collection[index] = new Random().Next(1, 10);
+    index++;
+  }
+}
+
+void PrintArray(int[] col)
+{
+  int count = col.Length;
+  int position = 0;
+  while (position < count)
+  {
+    Console.WriteLine(col[position]);
+    position++;
+  }
+}
+
+int IndexOf(int[] collection, int find)
+{
+  int count = collection.Length;
+  int index = 0;
+  int position = -1;
+  while (index < count)
+  {
+    if (collection[index] == find)
+    {
+      position = index;
+      break;
+    }
+    index++;
+  }
+  return position;
+}
+
+
+int[] array = new int[10];    //определили массив из 10 элементов
+
+
+FillArray(array);       // метод который заполняет массив
+PrintArray(array);      // распечатывает значения массива
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
